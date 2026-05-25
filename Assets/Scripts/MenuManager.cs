5adec833bb70496522a7061f2d907530dbb7760b
Albayrak-1 <240404022@ogr.kent.edu.tr>
2026-05-25 23:44:54 +0300

    Add Main Menu and finalize game loop UI

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,13 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void RestartGame()
+    public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene"); 
+        Cursor.lockState = CursorLockMode.Locked; 
+        SceneManager.LoadScene("MainScene"); 
     }
 
     public void GoToMainMenu()
     {
+        Cursor.lockState = CursorLockMode.None; 
         SceneManager.LoadScene("MainMenu"); 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); 
     }
 }
